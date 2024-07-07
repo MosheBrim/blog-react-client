0ed653c0b6d64cc67af04c3cb2bf757919eecb4d
MosheBrim <brim0533167854@gmail.com>
2024-07-07 15:22:43 +0300

    change service url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // serviceUrl: "http://localhost:8081"
-    serviceUrl: "https://blog-spring-server.onrender.com"
+    serviceUrl: "http://localhost:8081"
+    // serviceUrl: "https://blog-spring-server.onrender.com"
 };
 
 export default config;
